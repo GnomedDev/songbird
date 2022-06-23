ee265af8ec83dd71b3e6b681881a524fe701755b
Gnome <david2005thomas@gmail.com>
2022-06-23 14:07:46 +0100

    Support serenity@next instead of pinning version

--- src/id.rs
+++ src/id.rs
@@ -8,7 +8,10 @@ use serenity::model::id::{
     GuildId as SerenityGuild,
     UserId as SerenityUser,
 };
-use std::fmt::{Display, Formatter, Result as FmtResult};
+use std::{
+    fmt::{Display, Formatter, Result as FmtResult},
+    num::NonZeroU64,
+};
 #[cfg(feature = "twilight")]
 use twilight_model::id::{
     marker::{ChannelMarker, GuildMarker, UserMarker},
@@ -16,29 +19,22 @@ use twilight_model::id::{
 };
 
 /// ID of a Discord voice/text channel.
-#[derive(Clone, Copy, Debug, Default, Eq, Hash, PartialEq)]
-pub struct ChannelId(pub u64);
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct ChannelId(pub NonZeroU64);
 
 /// ID of a Discord guild (colloquially, "server").
-#[derive(Clone, Copy, Debug, Default, Eq, Hash, PartialEq)]
-pub struct GuildId(pub u64);
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct GuildId(pub NonZeroU64);
 
 /// ID of a Discord user.
-#[derive(Clone, Copy, Debug, Default, Eq, Hash, PartialEq)]
-pub struct UserId(pub u64);
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct UserId(pub NonZeroU64);
 
 impl Display for ChannelId {
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
         Display::fmt(&self.0, f)
     }
 }
-
-impl From<u64> for ChannelId {
-    fn from(id: u64) -> Self {
-        Self(id)
-    }
-}
-
 #[cfg(feature = "serenity")]
 impl From<SerenityChannel> for ChannelId {
     fn from(id: SerenityChannel) -> Self {
@@ -49,19 +45,13 @@ impl From<SerenityChannel> for ChannelId {
 #[cfg(feature = "twilight")]
 impl From<TwilightId<ChannelMarker>> for ChannelId {
     fn from(id: TwilightId<ChannelMarker>) -> Self {
-        Self(id.get())
+        Self(id.into_nonzero())
     }
 }
 
 impl Display for GuildId {
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
         Display::fmt(&self.0, f)
-    }
-}
-
-impl From<u64> for GuildId {
-    fn from(id: u64) -> Self {
-        Self(id)
     }
 }
 
@@ -75,26 +65,20 @@ impl From<SerenityGuild> for GuildId {
 #[cfg(feature = "driver")]
 impl From<GuildId> for DriverGuild {
     fn from(id: GuildId) -> Self {
-        Self(id.0)
+        Self(id.0.get())
     }
 }
 
 #[cfg(feature = "twilight")]
 impl From<TwilightId<GuildMarker>> for GuildId {
     fn from(id: TwilightId<GuildMarker>) -> Self {
-        Self(id.get())
+        Self(id.into_nonzero())
     }
 }
 
 impl Display for UserId {
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
         Display::fmt(&self.0, f)
-    }
-}
-
-impl From<u64> for UserId {
-    fn from(id: u64) -> Self {
-        Self(id)
     }
 }
 
@@ -108,13 +92,13 @@ impl From<SerenityUser> for UserId {
 #[cfg(feature = "driver")]
 impl From<UserId> for DriverUser {
     fn from(id: UserId) -> Self {
-        Self(id.0)
+        Self(id.0.get())
     }
 }
 
 #[cfg(feature = "twilight")]
 impl From<TwilightId<UserMarker>> for UserId {
     fn from(id: TwilightId<UserMarker>) -> Self {
-        Self(id.get())
+        Self(id.into_nonzero())
     }
 }
