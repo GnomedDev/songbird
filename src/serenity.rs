fd548957a6e4c42cdf28ba091a73143184f65a3f
David Thomas <david2005thomas@gmail.com>
2023-11-28 09:31:35 +0000

    Support serenity generic's data

--- src/serenity.rs
+++ src/serenity.rs
@@ -4,54 +4,24 @@
 //! [serenity]: https://crates.io/crates/serenity
 
 use crate::{Config, Songbird};
-use serenity::{
-    client::{ClientBuilder, Context},
-    prelude::TypeMapKey,
-};
+use serenity::client::ClientBuilder;
 use std::sync::Arc;
 
-/// Zero-size type used to retrieve the registered [`Songbird`] instance
-/// from serenity's inner [`TypeMap`].
-///
-/// [`Songbird`]: Songbird
-/// [`TypeMap`]: serenity::prelude::TypeMap
-pub struct SongbirdKey;
-
-impl TypeMapKey for SongbirdKey {
-    type Value = Arc<Songbird>;
-}
-
 /// Installs a new songbird instance into the serenity client.
-///
-/// This should be called after any uses of `ClientBuilder::type_map`.
 pub fn register(client_builder: ClientBuilder) -> ClientBuilder {
     let voice = Songbird::serenity();
     register_with(client_builder, voice)
 }
 
 /// Installs a given songbird instance into the serenity client.
-///
-/// This should be called after any uses of `ClientBuilder::type_map`.
 pub fn register_with(client_builder: ClientBuilder, voice: Arc<Songbird>) -> ClientBuilder {
-    client_builder
-        .voice_manager_arc(voice.clone())
-        .type_map_insert::<SongbirdKey>(voice)
+    client_builder.voice_manager_arc(voice)
 }
 
 /// Installs a given songbird instance into the serenity client.
-///
-/// This should be called after any uses of `ClientBuilder::type_map`.
 pub fn register_from_config(client_builder: ClientBuilder, config: Config) -> ClientBuilder {
     let voice = Songbird::serenity_from_config(config);
     register_with(client_builder, voice)
-}
-
-/// Retrieve the Songbird voice client from a serenity context's
-/// shared key-value store.
-pub async fn get(ctx: &Context) -> Option<Arc<Songbird>> {
-    let data = ctx.data.read().await;
-
-    data.get::<SongbirdKey>().cloned()
 }
 
 /// Helper trait to add installation/creation methods to serenity's
